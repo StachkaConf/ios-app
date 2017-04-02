c9dfd1af248d23185dc501498a50f4a3122d415d
Konstantin Mordan <konstantinmordan@gmail.com>
2017-04-02 13:23:24 +0300

    Добавил Constants

---
+++ StachkaIOS/StachkaIOS/Classes/Helpers/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  StachkaIOS
+//
+//  Created by Konstantin Mordan on 02/04/2017.
+//  Copyright © 2017 m.rakhmanov. All rights reserved.
+//
+
+import Foundation
+
+enum StoryboardIdentifier {
+    static let conferences = "Conferences"
+    static let main        = "Main"
+}
