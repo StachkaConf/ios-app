4419d600551dfdb37a37fa6878209577e4e74bdd
Konstantin Mordan <konstantinmordan@gmail.com>
2017-04-02 13:18:44 +0300

    Добавил OnboardingAssembly

---
+++ StachkaIOS/StachkaIOS/Classes/User Stories/Onboarding/Assembly/OnboardingAssembly.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingAssembly.swift
+//  StachkaIOS
+//
+//  Created by Konstantin Mordan on 02/04/2017.
+//  Copyright © 2017 m.rakhmanov. All rights reserved.
+//
+
+import Foundation
+
+class OnboardingAssembly: ModuleAssembly {
+    func module() -> UIViewController {
+        
+    }
+}
